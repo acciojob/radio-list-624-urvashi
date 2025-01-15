4cb46baaef326c37d166631d29c910331ff5bacf
624-urvashi <143706170+624-urvashi@users.noreply.github.com>
2025-01-15 13:30:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    
+    document.getElementById('option3').checked = true;
+
+   
+    const radioGroups = document.querySelectorAll('.radio-group');
+    radioGroups.forEach((group, index) => {
+        if ((index + 1) % 2 === 0) {
+            group.querySelector('input[type="radio"]').classList.add('input');
+        }
+    });
+});
+
